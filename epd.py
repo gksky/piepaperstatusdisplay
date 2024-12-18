467407d0d03f15ed6a039c739f11a0c4c7cfdad4
Alexander Gutkovsky <a.gutkovsky@gmail.com>
2024-12-18 15:49:57 +0300

    fix: font size

--- epd.py
+++ epd.py
@@ -177,7 +177,7 @@ epd.Clear(0xFF)
 font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 16)
 quote_font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 12)
 author_font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 10)
-clock_font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 48)
+clock_font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 64)
 #firstTime = True
 ip_image = Image.new('1', (epd.height, epd.width), 255)
 ip_draw = ImageDraw.Draw(ip_image)
@@ -227,7 +227,7 @@ while (True):
         ip_draw.text((EPD_WIDTH - (author_font.getsize(quote_data[1])[0]), EPD_HEIGHT - 12), quote_data[1], font = author_font, fill = 0)
     else:
         remaining_hours, remaining_minutes = time_until_friday_19()
-        ip_draw.text((0, 65), f"{remaining_hours - 3}:{remaining_minutes}", font = clock_font, fill = 0)
+        ip_draw.text((0, 60), f"{remaining_hours - 3}:{remaining_minutes}", font = clock_font, fill = 0)
 
     epd.displayPartial(epd.getbuffer(ip_image))
 
